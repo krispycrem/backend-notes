17dfb4d65a88d0ae353cfa38223bf7d674a28401
krispycrem <obananas073@gmail.com>
2024-04-25 15:49:28 +0300

    add connection to db with dotenv

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
+require('dotenv').config()
+const Note = require('./models/note')
 const express = require('express')
 const cors = require('cors')
-
+// const mongoose = require('mongoose')
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
+// if (process.argv.length<3) {
+//   console.log('give password as argument')
+//   process.exit(1)
+// }
+//
+// const password = process.argv[2]
 
+// const url =
+// `mongodb+srv://obananas073:${password}@database.3dhnoyy.mongodb.net/noteApp?`
+
+// mongoose.set('strictQuery',false)
+
+// mongoose.connect(url)
+
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   important: Boolean,
+// })
+
+// noteSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString()
+//     delete returnedObject._id
+//     delete returnedObject.__v
+//   }
+// })
+
+// const Note = mongoose.model('Note', noteSchema)
 
 
 let notes = [
@@ -48,7 +77,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 const generateId = () => {
@@ -98,7 +129,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
